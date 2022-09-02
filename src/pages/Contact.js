eb9e7b133f4d36b7ed42dea5f3a02405a2f7b5c5
KeemDev <66824881+KeemDev@users.noreply.github.com>
2022-09-03 02:23:18 +0300

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <div className="rightSide">
         <h1>Contact Him if you are interested!</h1>
 
-        <form id="contact-form" name="contact" method="POST" data-netlify="true">
+        <form id="contact-form" name="contact" method="POST" data-netlify="true" onSubmit="submit">
           <input type="hidden" name="form-name" value="contact" />
     
           <label htmlFor="name">Full Name</label>
